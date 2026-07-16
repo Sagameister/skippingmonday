2cfdd6bf4a6b71bb49f948fa2579bd65e74f8f49
Sagameister <86540738+Sagameister@users.noreply.github.com>
2026-07-16 07:57:45 +0200

    newsletter: create subscriber schema, register in index, build Vercel API /api/subscribe and hook up Footer form

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -2,5 +2,6 @@ import post from './post'
 import video from './video'
 import photo from './photo'
 import song from './song'
+import subscriber from './subscriber'
 
-export const schemaTypes = [post, video, photo, song]
+export const schemaTypes = [post, video, photo, song, subscriber]
